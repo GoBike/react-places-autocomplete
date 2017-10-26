e6ef8a32792c5bd39af219f2d76c27caa03f37d2
Yoong Jun Quan <yoongjunquan@gmail.com>
2017-10-26 18:32:28 +0800

    Update <input> -> <textarea>

--- src/PlacesAutocomplete.js
+++ src/PlacesAutocomplete.js
@@ -255,7 +255,7 @@ class PlacesAutocomplete extends Component {
         id="PlacesAutocomplete__root"
         style={this.inlineStyleFor('root')}
         className={this.classNameFor('root')}>
-        <input {...inputProps} />
+        {inputProps.lines && inputProps.lines > 1 ? <textarea rows={inputProps.lines} {...inputProps} /> : <input {...inputProps} />}
         {autocompleteItems.length > 0 && (
           <div
             id="PlacesAutocomplete__autocomplete-container"
